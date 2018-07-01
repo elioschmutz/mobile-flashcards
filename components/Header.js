624e094fb1d16e347dcd047138be7af5cb5e8972
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-01 22:43:49 +0200

    feat: Add primary and secondary colors

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { red, white } from '../utils/colors'
+import { primary, white } from '../utils/colors'
+import { select } from '../utils/platform'
 import { Header as RNEHeader } from 'react-native-elements'
+import { Ionicons } from '@expo/vector-icons'
 
 class Header extends Component {
-  render() {
-    const { children } = this.props
+  backButton = {
+    icon: select('ios-arrow-back', 'md-arrow-back'),
+    color: '#fff',
+    type: 'ionicon'
+  }
 
+  render() {
+    const { children, showBackButton } = this.props
     return (
       <RNEHeader
-        backgroundColor={red}
+        backgroundColor={primary}
         statusBarProps={{ barStyle: 'light-content' }}
+        leftComponent={showBackButton ? this.backButton : {}}
         centerComponent={{ text: children, style: { color: white } }}
       />
     )
