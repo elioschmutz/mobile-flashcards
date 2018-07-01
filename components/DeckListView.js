501595f4f7d37e5e28ddc0b1cf2799280e5952e3
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-01 21:33:16 +0200

    feat: Add DeckListView component

---
+++ components/DeckListView.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text, FlatList } from 'react-native'
+import Header from './Header'
+import { ListItem } from 'react-native-elements'
+
+const DeckListViewItem = ({ title, questionLength }) => (
+  <ListItem title={title} subtitle={`${questionLength} cards`} />
+)
+
+class DeckListView extends Component {
+  state = {
+    decks: {
+      React: {
+        title: 'React',
+        questions: [
+          {
+            question: 'What is React?',
+            answer: 'A library for managing user interfaces'
+          },
+          {
+            question: 'Where do you make Ajax requests in React?',
+            answer: 'The componentDidMount lifecycle event'
+          }
+        ]
+      },
+      JavaScript: {
+        title: 'JavaScript',
+        questions: [
+          {
+            question: 'What is a closure?',
+            answer:
+              'The combination of a function and the lexical environment within which that function was declared.'
+          }
+        ]
+      }
+    }
+  }
+
+  _renderItem = ({ item }) => {
+    return <DeckListViewItem title={item.title} questionLength={item.questions.length} />
+  }
+
+  render() {
+    const { decks } = this.state
+
+    return (
+      <View style={styles.container}>
+        <Header>Decks</Header>
+        <FlatList
+          data={Object.values(decks)}
+          renderItem={this._renderItem}
+          extraData={this.state}
+          keyExtractor={(item, index) => item.title}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-around'
+  },
+  item: {
+    alignItems: 'center'
+  }
+})
+
+export default DeckListView
