9e35f448c0d9e16890dd9b77e07fca29d274fed6
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-03 19:18:32 +0200

    feat: Add AddDeckView component

---
+++ components/AddDeckView.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import Header from './Header'
+import { Text } from 'react-native-elements'
+import { PrimaryButton } from './Button'
+import { FormInput } from 'react-native-elements'
+
+class AddDeckView extends Component {
+  state = {
+    text: ''
+  }
+  render() {
+    const { max, score } = this.props
+
+    return (
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+        <Header showBackButton={true}>New Deck</Header>
+        <View style={[styles.container]}>
+          <Text h1>What is the title of your new deck?</Text>
+          <FormInput
+            inputStyle={{}}
+            placeholder="Please enter a title"
+            onChangeText={text => this.setState({ text })}
+            value={this.state.text}
+          />
+
+          <PrimaryButton title="Submit" />
+        </View>
+      </KeyboardAvoidingView>
+    )
+  }
+}
+
+export default AddDeckView
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    padding: 20
+  },
+})
