d96b597a3af942bcddeb9e12c2e902e5efee7fd2
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-13 14:59:54 +0200

    style: Change secondary color

--- utils/colors.js
+++ utils/colors.js
@@ -2,14 +2,14 @@ export const purple = '#292477'
 export const gray = '#757575'
 export const white = '#fff'
 export const red = '#b71845'
-export const orange = '#f26f28'
+export const orange = '#DDA448'
 export const blue = '#4e4cb8'
 export const lightPurp = '#7c53c3'
 export const pink = '#b93fb3'
 export const green = '#398563'
 
 export const primary = red
-export const secondary = gray
+export const secondary = orange
 
 export const success = green
 export const error = red
