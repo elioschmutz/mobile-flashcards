9a835e098c430331f941cf6007e8907cd3e89012
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-07-13 14:35:32 +0200

    feat: Implement AddDeckView to store deck in redux and asyncstorage

--- components/AddDeckView.js
+++ components/AddDeckView.js
@@ -3,20 +3,45 @@ import { View, StyleSheet, KeyboardAvoidingView } from 'react-native'
 import { Text } from 'react-native-elements'
 import { PrimaryButton } from './Button'
 import { FormInput } from 'react-native-elements'
+import { addDeck } from '../actions/decks'
+import { connect } from 'react-redux'
 
 class AddDeckView extends Component {
-  state = {
-    text: ''
+  initState = {
+    title: ''
   }
-  onSubmit = () => {
-    const { navigation } = this.props
 
-    navigation.push('Home')
-    navigation.navigate('DeckView', { deckId: 1 })
+  state = {
+    title: ''
+  }
+
+  onSubmit = () => {
+    const { navigation, dispatch } = this.props
+    const { title } = this.state
+
+    if (!this.validate()) {
+      return
+    }
+    this.reset()
+    dispatch(addDeck(title))
+    navigation.navigate('DeckView', { deckId: title })
+  }
+
+  reset = () => {
+    this.setState(this.initState)
+  }
+
+  validate = () => {
+    const { title } = this.state
+
+    if (title == '') {
+      return false
+    }
+    return true
   }
 
   render() {
-    const { max, score } = this.props
+    const { title } = this.state
 
     return (
       <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
@@ -25,18 +50,16 @@ class AddDeckView extends Component {
           <FormInput
             inputStyle={{}}
             placeholder="Please enter a title"
-            onChangeText={text => this.setState({ text })}
-            value={this.state.text}
+            onChangeText={title => this.setState({ title })}
+            value={title}
           />
 
-          <PrimaryButton onPress={this.onSubmit} title="Submit" />
+          <PrimaryButton disabled={!this.validate()} onPress={this.onSubmit} title="Submit" />
         </View>
       </KeyboardAvoidingView>
     )
   }
 }
-
-export default AddDeckView
 
 const styles = StyleSheet.create({
   container: {
@@ -45,3 +68,5 @@ const styles = StyleSheet.create({
     padding: 20
   }
 })
+
+export default connect()(AddDeckView)
